70a19a035cd5c08807e6490b2c968c530a81af38
Adam Malone <adam@adammalone.net>
2013-08-02 11:44:08 +1000

    Completed the script to a basic level

--- perl_github.pl
+++ perl_github.pl
@@ -1,3 +1,28 @@
 #!/usr/bin/perl
 
-print "Initial commit to spark the repo"
+use HTTP::Request;
+use LWP::UserAgent;
+
+# Repo details
+my $github_user = 'typhonius';
+my $github_repo = 'perl_github';
+my $domain = "https://api.github.com/repos/$github_user/$github_repo/issues";
+#my $token = 'FILL THIS IN';
+
+# Issue Details
+my $title = 'nothing works';
+my $body = 'everything am bug';
+
+my $ua = LWP::UserAgent->new;
+my $req = HTTP::Request->new(POST => $domain);
+my $post_data = '{ "title": "' . $title . '", "body": "' . $body . '" }';
+$req->content($post_data);
+$req->header('Authorization' => "bearer " . $token);
+my $res = $ua->request($req);
+
+if ($res->is_success) {
+  print $res->content;
+}
+else {
+  print $res->content;
+}
